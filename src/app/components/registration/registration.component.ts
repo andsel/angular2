958452d680deb5be2692611423ed7e01f7305b4d
osboxes.org <selva.andre@gmail.com>
2017-03-28 10:57:15 +0100

    Added form validation using directive

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface RegistrationForm {
+  name: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  password: string;
+  repeatPassword: string;
+  privacyTerms: boolean;
+}
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+
+  form : RegistrationForm = {
+    name: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    password: '',
+    repeatPassword: '',
+    privacyTerms: false,
+  }
 
   constructor() { }
 
